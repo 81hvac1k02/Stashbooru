d7d0be004a2d246f6a6f6ef4d9dddcd441b5fce2
81hvac1k02 <125800300+81hvac1k02@users.noreply.github.com>
2024-05-24 11:22:55 +0200

    Update stashbooru.py
    Updated and refactored code

--- stashbooru.py
+++ stashbooru.py
@@ -9,32 +9,34 @@ from stashapi.stashapp import StashInterface
 
 
 class ServerInfo:
-    deepbooru_port_nr = 7860
-    stash_port = 9999
     deepbooru_domain = "deepbooru"
     stash_domain = "stash"
-    deepbooru_url= f"http://{deepbooru}:{deepbooru_port_nr}/api/predict"
-    api_key = "" # make sure to add a api key 
+    stash_port_nr = 9999
+    deepbooru_port_nr = 7860
+    DEEPBOORU_URL = f"http://{deepbooru_domain}:{deepbooru_port_nr}/api/predict"  # replace with your API URL
+    stash_api_key = ""
     stashargs = {
         "scheme": "http",
-        "host": "stash",
-        "port": "9999",
+        "host": stash_domain,
+        "port": stash_port_nr,
         "logger": log,
-        "ApiKey": api_key
+        "ApiKey": stash_api_key
     }
     stash = StashInterface(stashargs)
 
 
 def get_untagged_images(return_amount=5) -> list:
-    # return_amount = 5
+
     less_than_amount = 3
+    page_nr = 2
 
     tag_count = {'tag_count': {'modifier': "LESS_THAN", 'value': less_than_amount}}
 
     query = {
         "per_page": return_amount,
         "sort": "created_at",
-        "direction": "ASC"
+        "direction": "ASC",
+        "page": page_nr
     }
 
     tag_is_null = {
@@ -58,30 +60,29 @@ def get_untagged_images(return_amount=5) -> list:
     return image_ids
 
 
-def get_existing_tag_ids(image_id: int) -> list:
-    existing_tags = []
+def get_existing_tag_ids(image_id: int) -> list[str]:
     current_ids = ServerInfo.stash.find_image(image_in=image_id, fragment='tags{id}')
-    for dicts in current_ids['tags']:
-        existing_tags.append(dicts.get('id'))
+    existing_tags = [dicts.get('id') for dicts in current_ids['tags']]
     return existing_tags
 
 
-def strip_tags(tags, image_id: int) -> list:
-    if len(tags) != 0:
-        old_tag_ids = get_existing_tag_ids(image_id)
+async def strip_tags(in_tags: tuple, image_id: int) -> list[int] | None:
+    if len(in_tags) != 0:
+        old_tag_ids = set(map(int, get_existing_tag_ids(image_id)))
 
-        new_tags = [z.strip() for x in tags for z in x.split(',')]
+        new_tags = [z.strip() for x in in_tags for z in x.split(',')]
 
-        new_tag_ids = ServerInfo.stash.map_tag_ids(tags_input=new_tags, create=True)
+        new_tag_ids = set(map(int, ServerInfo.stash.map_tag_ids(tags_input=new_tags, create=False)))
 
-        tag_ids = old_tag_ids + new_tag_ids
-
-        return tag_ids
+        if old_tag_ids == new_tag_ids:
+            return None
+        else:
+            return list(old_tag_ids | new_tag_ids)
     return None
 
 
 async def encode_image(image_id: int) -> str:
-    image_url = f"http://stash:9999/image/{image_id}/thumbnail?apikey={ServerInfo.api_key}"
+    image_url = f"http://{ServerInfo.stash_domain}:{ServerInfo.stash_port_nr}/image/{image_id}/thumbnail?apikey={ServerInfo.stash_api_key}"
     async with aiohttp.ClientSession() as session:
         async with session.get(image_url) as response:
             if response.status == 200:
@@ -91,17 +92,18 @@ async def encode_image(image_id: int) -> str:
                 return image_base64_string
 
 
-async def update_image_tags(*args, image_id: int):
-    tag_ids = strip_tags(args, image_id)
+async def update_image_tags(*args, image_id: int) -> None:
+    tag_ids = await strip_tags(args, image_id)
+
     if tag_ids is not None:
         try:
             log.info(f"updating image: {image_id}")
             ServerInfo.stash.update_image({'id': image_id, 'tag_ids': tag_ids})
-        except Exception as err:
-            print(f'Error occurred: {err}')
+        except Exception as update_err:
+            print(f'Error occurred with image: {image_id}: {update_err}')
 
 
-async def post_request(image_id: int, threshold=0.6):
+async def post_request(image_id: int, threshold=0.6) -> None:
     image = await encode_image(image_id)
     data = {
         "data": [image, threshold]
@@ -109,17 +111,19 @@ async def post_request(image_id: int, threshold=0.6):
 
     async with aiohttp.ClientSession() as session:
         async with session.post(
-                ServerInfo.REALBOORU_API_URL,
+                ServerInfo.DEEPBOORU_URL,
                 headers={"Content-Type": "application/json; charset=utf-8"},
                 data=json.dumps(data)
         ) as response:
-            response_json = await response.json()
+            try:
+                response_json = await response.json()
+                if 'data' in response_json:
+                    response_data = response_json['data'][2]
 
-            response_data = response_json['data'][2]
+                    await update_image_tags(response_data, image_id=image_id)
 
-            update = await update_image_tags(response_data, image_id=image_id)
-
-            # return response_json
+            except Exception as response_err:
+                print(f'Error occurred: {response_err}')
 
 
 async def handle_multiple_image_ids(image_ids: list) -> tuple:
@@ -128,15 +132,14 @@ async def handle_multiple_image_ids(image_ids: list) -> tuple:
     return results
 
 
-def main():
+def main(minutes=10):
     # the script updates about 100 images per minute
     # the minute variable defines the execution time
-    minutes = 1
-    # for m in range(minutes):
-    while (True):
+    
+    for m in range(minutes):
         image_ids = get_untagged_images(100)
-        # image_ids = [6348]
-        test = asyncio.run(handle_multiple_image_ids(image_ids))
+
+        asyncio.run(handle_multiple_image_ids(image_ids))
 
 
 if __name__ == "__main__":
